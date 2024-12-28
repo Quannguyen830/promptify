f642e190c47c1ac8d3729e052dd41cb8347d75f0
vietquanRMIT <nguyenvietquan512@gmail.com>
2024-12-28 13:56:19 +1100

    upload function works

---
+++ src/app/api/upload/route.ts
@@ -0,0 +1,55 @@
+import { type NextRequest, NextResponse } from 'next/server';
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
+import { string } from 'zod';
+
+interface UploadFileParams {
+  fileBuffer: Buffer;
+  fileName: string;
+  mimetype: string;
+}
+
+const bucketName = process.env.AWS_BUCKET_NAME
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY
+const secretAccessKey = process.env.AWS_SECRET
+
+if (!bucketName || !region || !accessKeyId || !secretAccessKey) {
+  throw new Error('Missing AWS credentials');
+}
+
+const s3Client = new S3Client({
+  region,
+  credentials: {
+    accessKeyId,
+    secretAccessKey
+  }
+})
+
+export async function POST(request: NextRequest) {
+  try {
+    const formData = await request.formData();
+    const file = formData.get("file") as File;
+    const fileBuffer = await file.arrayBuffer();
+    const fileName = formData.get("caption") as string;
+    const mimetype = file.type;
+
+    const response = await uploadFile(Buffer.from(fileBuffer), fileName, mimetype);
+    return NextResponse.json({ message: 'File uploaded successfully', response });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: 'File upload failed', error: error.message }, { status: 500 });
+    }
+    return NextResponse.json({ message: 'File upload failed', error: 'Unknown error' }, { status: 500 });
+  }
+}
+
+function uploadFile(fileBuffer: Buffer, fileName: string, mimetype: string) {
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileBuffer,
+    Key: fileName,
+    ContentType: mimetype
+  }
+
+  return s3Client.send(new PutObjectCommand(uploadParams));
+}
--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -3,10 +3,35 @@
 import React, { useState } from 'react'
 import { Navbar } from '~/components/share/navbar'
 import { SidebarNav } from '~/components/share/sidebarComponent'
+import { Button } from '~/components/ui/button'
 import { cn } from '~/lib/utils'
 
+interface UploadResponse {
+  message: string;
+  response?: string; // This is the S3 response
+  error?: string;
+}
+
+
 export default function Page() {
-  // const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+
+  const handleUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (!file) return;
+
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("caption", file.name);
+
+    const response = await fetch('/api/upload', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const result = await response.json() as UploadResponse;
+    console.log(result);
+  }
 
   // useEffect(() => {
   //   if (isSidebarOpen) {
@@ -24,12 +49,27 @@ export default function Page() {
       <div className="flex flex-1 overflow-hidden h-full">
         <aside
           className={cn(
-            "w-1/6 border-r bg-background transition-all duration-300 ease-in-out"
-            // isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+            "w-1/6 border-r bg-background transition-all duration-300 ease-in-out",
+            isSidebarOpen ? "translate-x-0" : "-translate-x-full"
           )}
         >
           <SidebarNav />
         </aside>
+        <main className="flex-1 p-4">
+          {/* Upload Button */}
+          <div className="flex justify-center items-center h-full">
+            <input type="file" onChange={handleUpload} className="hidden" id="fileInput" />
+            <label htmlFor="fileInput">
+              <Button
+                variant={"secondary"}
+                className='p-5 text-xl'
+                onClick={() => document.getElementById('fileInput')?.click()}
+              >
+                Upload Files
+              </Button>
+            </label>
+          </div>
+        </main>
       </div>
     </div>
   )
