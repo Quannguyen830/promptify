f6d2292b3f7e381afadb888040fa0e437a2cb1f0
dinh-thang <64820925+dinh-thang@users.noreply.github.com>
2025-01-04 11:50:47 +1100

    temp

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -24,43 +24,8 @@ export default function Page() {
   }, [isSidebarOpen])
 
   return (
-    // <div className="h-screen w-full flex flex-col bg-background">
-    //   <div className="flex flex-1 overflow-hidden h-full">
-    //     <Sidebar/>
-
-    //     <ResizablePanelGroup
-    //       direction="horizontal"
-    //       className={cn(
-    //         "flex-1 transition-all duration-300 ease-in-out"
-    //       )}
-    //       // onLayout={(sizes) => {
-    //       //   setMainPanelSize(sizes[0] ?? 0)
-    //       //   setChatPanelSize(sizes[1] ?? 0)
-    //       // }}
-    //     >
-    //       <ResizablePanel defaultSize={mainPanelSize} minSize={30}>
-    //         <div className="flex h-full flex-col">
-    //           <div className="flex items-center border-b-[2px] p-3">
-    //             <SidebarTrigger
-    //               onClick={() => setIsSidebarOpen(!isSidebarOpen)}
-    //               className="mr-4"
-    //             />
-    //             <h1 className="text-2xl font-semibold">Dashboard</h1>
-    //           </div>
-    //           <MainContent />
-    //         </div>
-    //       </ResizablePanel>
-
-    //       <ResizableHandle withHandle />
-
-    //       <ResizablePanel defaultSize={chatPanelSize} minSize={20}>
-    //         <ChatSection />
-    //       </ResizablePanel>
-    //     </ResizablePanelGroup>
-    //   </div>
-    // </div>
     <div>
-      
+      dashboard
     </div>
   )
 }
