2eec7031c26f46f90736b16224d4f1d7452618bf
vietquanRMIT <nguyenvietquan512@gmail.com>
2025-01-16 10:56:31 +0700

    reformat code

---
+++ src/config/S3-client.ts
@@ -0,0 +1,17 @@
+import { S3Client } from "@aws-sdk/client-s3"
+
+const region = process.env.AWS_BUCKET_REGION
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_SECRET
+
+if (!region || !accessKeyId || !secretAccessKey) {
+  throw new Error('Missing AWS credentials');
+}
+
+export const s3Client = new S3Client({
+  region,
+  credentials: {
+    accessKeyId,
+    secretAccessKey
+  }
+})
--- src/services/uploadService.ts
+++ src/services/uploadService.ts
@@ -2,22 +2,27 @@ import { type Session } from "next-auth";
 import { type UploadResponse } from "~/interface";
 
 export const uploadFile = async (session: Session, event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    if (!file) return;
+  const file = event.target.files?.[0];
+  if (!file) return;
 
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("caption", file.name);
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("caption", file.name);
 
-    if (session?.user.id) {
-      formData.append("userId", session.user.id);
-    }
-
-    const response = await fetch('/api/upload', {
-      method: 'POST',
-      body: formData,
-    });
-
-    const result = await response.json() as UploadResponse;
-    console.log(result);
+  if (session?.user.id) {
+    formData.append("userId", session.user.id);
   }
+
+  const response = await fetch('/api/upload', {
+    method: 'POST',
+    body: formData,
+  });
+
+  const result = await response.json() as UploadResponse;
+  console.log(result);
+}
+
+export const getFiles = () => {
+  
+  console.log("Hello")
+}
