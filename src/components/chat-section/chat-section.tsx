51c4133211b7d32505c5acfdff15cfad327382ae
dinh-thang <64820925+dinh-thang@users.noreply.github.com>
2025-01-20 15:39:46 +1100

    TMP

--- src/components/chat-section/chat-section.tsx
+++ src/components/chat-section/chat-section.tsx
@@ -1,20 +1,22 @@
 "use client"
 
 import { Button } from "~/components/ui/button"
-import { Input } from "~/components/ui/input"
 import { Send } from 'lucide-react'
-import { UserMessageBox } from "./user-message-box"
-import { AgentMessageBox } from "./agent-message-box"
 import { ChatBubble, ChatBubbleAvatar, ChatBubbleMessage } from "../ui/chat/chat-bubble"
 import { ChatInput } from "../ui/chat/chat-input"
 import ChatMessageList from "./chat-message-list"
+import { useState } from "react"
 
 export function ChatSection() {
+  // TODO: create separate context management
+
+
   return (
     <div className="flex flex-col h-full p-5">
       {/* Tab header */}
       <h2 className="h-7 font-semibold text-2xl mb-5">Chat</h2>
       
+      {/* TODO: group this to a component */}
       <ChatMessageList className="overflow-y-auto h-full">
         <ChatBubble>
           <ChatBubbleAvatar />
@@ -32,15 +34,15 @@ export function ChatSection() {
         </ChatBubble>
       </ChatMessageList>
 
-      <div className="">
-        <form className="flex gap-2 ">
-          <ChatInput/>
-          <Button size="icon" className="h-8 w-8">
-            <Send className="h-4 w-4" />
-            <span className="sr-only">Send message</span>
-          </Button>
-        </form>
-      </div>
+      <form className="flex gap-2 ">
+        {/* <ChatInput/> */}
+
+        <input type="text" className="ring-1 w-full"/>
+        <Button size="icon" className="h-8 w-8">
+          <Send className="h-4 w-4" />
+          <span className="sr-only">Send message</span>
+        </Button>
+      </form>
     </div>
   )
 }
