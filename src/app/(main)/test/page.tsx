14ff7f6cdaa6325d0548aa760c6b49275bc2a6ac
vietquanRMIT <nguyenvietquan512@gmail.com>
2025-03-04 19:03:13 +0700

    Testing 456

--- src/app/(main)/test/page.tsx
+++ src/app/(main)/test/page.tsx
@@ -1,57 +1,47 @@
 'use client'
 
 import { DocumentEditorContainerComponent, Toolbar } from '@syncfusion/ej2-react-documenteditor';
-import { useQuery } from '@tanstack/react-query';
-import Loading from '~/components/share/loading-spinner';
-import { api } from '~/trpc/react';
 import { useRef } from 'react';
 import { Button } from '~/components/ui/button';
 
 DocumentEditorContainerComponent.Inject(Toolbar);
 
-interface DocumentResponse {
-  // Add specific type definition based on your API response
-  content: string;
-  // other properties...
-}
-
 export default function TestPage() {
   const containerRef = useRef<DocumentEditorContainerComponent>(null);
-  const { data, isLoading, error } = api.file.getFileByFileId.useQuery({
-    fileId: 'cm77k2yzh0001l9ja0bm1g1b9'
-  });
 
-  const handleOpenDocument = async (): Promise<void> => {
-    const response = await fetch('http://localhost:3000/api/ping/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json() as DocumentResponse;
-
-    if (containerRef.current?.documentEditor) {
-      containerRef.current.documentEditor.open(data.content);
-    } else {
-      throw new Error('Document editor not initialized');
-    }
+  function load(): void {
+    fetch(
+      'http://localhost:6002/api/documenteditor/LoadFromS3',
+      {
+        method: 'Post',
+        headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+        body: JSON.stringify({ documentName: 'cm77k2yzh0001l9ja0bm1g1b9' })
+      }
+    )
+      .then(response => {
+        if (response.status === 200 || response.status === 304) {
+          return response.json();
+        } else {
+          throw new Error('Error loading data');
+        }
+      })
+      .then(json => {
+        if (containerRef.current) {
+          containerRef.current.documentEditor.open(JSON.stringify(json));
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
-
-  if (isLoading) return <Loading />
-  if (error) return <div>Error: {error.message}</div>
 
   return (
     <div className="h-full">
-      <Button onClick={handleOpenDocument}>Open Document From AWS S3 Bucket</Button>
+      <Button onClick={load}>Open Document From AWS S3 Bucket</Button>
       <DocumentEditorContainerComponent
         id="container"
         height='100%'
-        // serviceUrl="http://localhost:3000/api/ping/"
+        serviceUrl="http://localhost:6002/api/documenteditor"
         enableToolbar={true}
         ref={containerRef}
       />
