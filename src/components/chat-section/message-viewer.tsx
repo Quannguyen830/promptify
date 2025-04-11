6b642a3c89dd23a03cd2bd4669099ca006223475
dinh-thang <64820925+dinh-thang@users.noreply.github.com>
2025-04-11 17:23:14 +1000

    add auto scrolling

--- src/components/chat-section/message-viewer.tsx
+++ src/components/chat-section/message-viewer.tsx
@@ -4,13 +4,15 @@ import { ChatState, useChat } from "./chat-store"
 
 import ChatBubble from "./chat-bubble";
 import Loading from "../share/loading-spinner";
+import { useEffect, useRef } from "react";
 
 const MessageViewer: React.FC<BaseProps> = ({ className }) => {
+  const bottomRef = useRef<HTMLDivElement>(null);
   const {
     chatState,
     selectedSessionId
   } = useChat();
-  
+
   const { data: chatSession, isLoading } = api.chat.getChatSessionById.useQuery(
     {
       id: selectedSessionId!
@@ -19,6 +21,10 @@ const MessageViewer: React.FC<BaseProps> = ({ className }) => {
       enabled: chatState === ChatState.SESSION_SELECTED
     }
   )
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [chatSession?.messages]);
   
   if (isLoading) {
     return (
@@ -31,6 +37,8 @@ const MessageViewer: React.FC<BaseProps> = ({ className }) => {
       {chatSession?.messages.map((message, index) => (
         <ChatBubble content={message.content} key={index} variant={message.sender} />
       ))}
+      
+      <div ref={bottomRef}/>
     </div>
   )
 }
